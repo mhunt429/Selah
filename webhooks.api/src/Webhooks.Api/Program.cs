49a27db91345ed88fb4a2a496621a6c603362052
Michael Hunt <mhunt429@gmail.com>
2025-09-23 21:26:51 -0400

    fix namespance for metrics

--- webhooks.api/src/Webhooks.Api/Program.cs
+++ webhooks.api/src/Webhooks.Api/Program.cs
@@ -25,14 +25,14 @@ builder.Services.AddOpenTelemetry()
             .AddOtlpExporter()
             .AddMeter("Microsoft.AspNetCore.Hosting",
                 "Microsoft.AspNetCore.Server.Kestrel",
-                "selah-webapi")
+                "selah-webhooks-api")
             .AddView("request-duration",
                 new ExplicitBucketHistogramConfiguration
                 {
                     Boundaries = new[]
                         { 0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10 }
                 }).AddPrometheusExporter();
-        ;
+        
     });
 
 builder.Services.AddMetrics();
