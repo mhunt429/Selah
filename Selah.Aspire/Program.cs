2ec624c373ea8128da052b24ffc6dc32ce40f9bd
Michael Hunt <mhunt429@gmail.com>
2025-10-16 14:52:14 -0400

    remove webhooks project

--- Selah.Aspire/Program.cs
+++ Selah.Aspire/Program.cs
@@ -1,6 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.WebApi>("selah-webapi");
-//builder.AddProject<Projects.Webhooks_Api>("selah-webhooks-api");
 
 builder.Build().Run();
--- webhooks.api/src/Webhooks.Core/Config/AwsMessageSettings.cs
+++
@@ -1,6 +0,0 @@
-namespace Webhooks.Core.Config;
-
-public class AwsMessageSettings
-{
-    public required string PlaidWebhookTopic { get; set; }
-}
