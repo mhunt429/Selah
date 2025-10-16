78906d1417c83aa4fb0983213681e70ee475c07f
Michael Hunt <mhunt429@gmail.com>
2025-10-16 15:15:48 -0400

    saving models

---
+++ selah-angular/src/app/core/models/connector/plaidLinkTokenRequest.ts
@@ -0,0 +1,6 @@
+export type PlaidLinkTokenRequest = {
+  publicToken: string;
+  userId: number;
+  institutionName: string;
+  institutionId: string;
+};
---
+++ selah-angular/src/app/core/models/identity/accessToken.ts
@@ -0,0 +1,6 @@
+export type AccessToken = {
+  accessToken: string;
+  refreshToken: string;
+  accessTokenExpiration: string;
+  refreshTokenExpiration: string;
+};
