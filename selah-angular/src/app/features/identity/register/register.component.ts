d039f75eaa3e81acf21553c9e7067a39cb432e5a
Michael Hunt <mhunt429@gmail.com>
2025-10-17 14:56:07 -0400

    more registration styles

--- selah-angular/src/app/features/identity/register/register.component.ts
+++ selah-angular/src/app/features/identity/register/register.component.ts
@@ -1,14 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { FloatingLabelComponent } from '../../../shared/inputs/floating-label/floating-label.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
-  imports: [FloatingLabelComponent],
+  imports: [FloatingLabelComponent, ReactiveFormsModule],
 })
 export class RegisterComponent implements OnInit {
+  registrationForm = new FormGroup({
+    accountName: new FormControl(''),
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    emailAddress: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+    passwordConfirmation: new FormControl('', [Validators.required]),
+  });
+
   constructor() {}
 
   ngOnInit() {}
+
+  registrationSubmit(event: Event) {
+    event.preventDefault();
+  }
 }
--- selah-angular/src/app/shared/inputs/floating-label/floating-label.component.ts
+++ selah-angular/src/app/shared/inputs/floating-label/floating-label.component.ts
@@ -1,9 +1,18 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, forwardRef, Input, OnInit } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-floating-label',
   templateUrl: './floating-label.component.html',
   styleUrls: ['./floating-label.component.scss'],
+  imports: [ReactiveFormsModule],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => FloatingLabelComponent),
+      multi: true,
+    },
+  ],
 })
 export class FloatingLabelComponent implements OnInit {
   @Input() id: string = '';
@@ -12,7 +21,29 @@ export class FloatingLabelComponent implements OnInit {
   @Input() type: string = '';
   @Input() required: boolean = false;
 
+  value?: number | string | Date = undefined;
+
   constructor() {}
 
   ngOnInit() {}
+
+  private onChange: any = () => {};
+  private onTouched: any = () => {};
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  onInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.value = input.value;
+    this.onChange(this.value);
+  }
 }
